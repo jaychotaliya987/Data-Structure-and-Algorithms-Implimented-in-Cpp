6dda63a68b45fac06655bd4a521cbd41b5c3fc74
jaychotaliya987 <jaychotaliya987@gmail.com>
2024-04-02 16:21:03 +0200

    First commit

--- 1. LinkedLists/linked_list.cpp
+++ 1. LinkedLists/linked_list.cpp
@@ -3,17 +3,47 @@
 
 struct Node {
     int data;
-    struct Node* next; // ! This is POINTER TO STRUCT named next.
+    Node *next; //! node pointer named next
 };
 
-struct Node* head;
+Node *head;
 
-void Insert (int data, int n){
+void Insert(int data, int n)
+{
+    Node *temp1 = new Node();
+    temp1->data = data;
+    temp1->next = NULL;
+    if(n == 1){
+        temp1->next = head;
+        head = temp1;
+        return;
+    }
+    Node *temp2 = head;
+    for(int i = 0; i < n-2; i++){
+        temp2 = temp2->next;
+    }
+    temp1->next = temp2->next;
+    temp2->next = temp1;
+}
 
-    Node* temp1 = new Node ();
-
-};
+void Print(){
+    Node *temp = head;
+    while(temp != NULL){
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+}
 
 int main(){
-    std::cout << "hello" << std::endl;
+    head = NULL;
+    Insert(2,1);
+    Print();
+    Insert(3,2);
+    Print();
+    Insert(4,3);
+    Print();
+    Insert(5,4);
+    Print();
+    return 0;
 };
